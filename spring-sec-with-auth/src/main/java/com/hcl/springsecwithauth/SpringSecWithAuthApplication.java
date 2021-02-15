2a98be3ffa56d8509369a497ff8352a82ce568de
brilala <briana.lerma@gmail.com>
2021-02-15 11:19:55 -0600

    Spring MVC with Authorization Submit

---
+++ spring-sec-with-auth/src/main/java/com/hcl/springsecwithauth/SpringSecWithAuthApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.springsecwithauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecWithAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecWithAuthApplication.class, args);
+	}
+
+}
---
+++ spring-sec-with-auth/src/test/java/com/hcl/springsecwithauth/SpringSecWithAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.springsecwithauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecWithAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
